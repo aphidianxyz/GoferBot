a84ca89c86ab40f7aea6e263d3b6f86b19235b03
aphidianxyz <aphidiangithub@proton.me>
2025-03-07 20:16:02 -0500

    /caption now deletes file from local storage after sending

--- main.go
+++ main.go
@@ -93,7 +93,7 @@ func main() {
             bot.Send(msgConfig)
         } else if msg.Photo != nil { // for commands that have an attached photo; any text attached w/ a photo
                                      // is considered as a caption (so we can't check if it's a cmd)
-            var msgConfig telebot.MessageConfig
+            var sendConfig telebot.Chattable
             tokens := strings.Split(msg.Caption, " ")
             commandName := tokens[0]
             if len(commandName) == 0 {
@@ -109,38 +109,44 @@ func main() {
             case "/caption": 
                 if len(commandParams) < 1 {
                     helpString := `Correct usage: /caption ["Top Text"] ["Bottom Text"] (Brackets not included)`
-                    msgConfig = telebot.NewMessage(msg.Chat.ID, helpString)
+                    sendConfig = telebot.NewMessage(msg.Chat.ID, helpString)
                     break
                 }
                 photos := *msg.Photo
                 // msg.Photo is a slice of PhotoSizes from the TG API which are offerings of the same
                 // photo in various sizes
+                // TODO: find biggest photo in PhotoSizes
                 targetFileID := photos[1].FileID
                 targetFileURL, err := bot.GetFileDirectURL(targetFileID)
                 if err != nil {
                     failedImgDownloadError := "Failed to get URL to download image - " + err.Error()
-                    msgConfig = telebot.NewMessage(msg.Chat.ID, failedImgDownloadError)
+                    sendConfig = telebot.NewMessage(msg.Chat.ID, failedImgDownloadError)
                     break
                 }
                 imgFilePath, err := DownloadImage(targetFileURL)
                 if err != nil {
                     failedDownloadError := "Failed to download image - " + err.Error()
-                    msgConfig = telebot.NewMessage(msg.Chat.ID, failedDownloadError)
+                    sendConfig = telebot.NewMessage(msg.Chat.ID, failedDownloadError)
                     break
                 }
-                ready := fmt.Sprintf("Image downloaded and ready to caption: %v", imgFilePath)
                 err = CaptionImage(imgFilePath, commandParams[0], commandParams[1])
                 if err != nil {
                     failedToCaptionImg := "Failed to caption image - " + err.Error()
-                    msgConfig = telebot.NewMessage(msg.Chat.ID, failedToCaptionImg)
+                    sendConfig = telebot.NewMessage(msg.Chat.ID, failedToCaptionImg)
                     break
                 }
-                msgConfig = telebot.NewMessage(msg.Chat.ID, ready)
+                // delete photo after sending
+                photoConfig := telebot.NewPhotoUpload(msg.Chat.ID, imgFilePath)
+                sendConfig = photoConfig 
+                bot.Send(sendConfig)
+                os.Remove(imgFilePath)
+                continue
             default:
-                msgConfig = telebot.NewMessage(msg.Chat.ID, "Unknown command")
+                msgConfig := telebot.NewMessage(msg.Chat.ID, "Unknown command")
                 msgConfig.ReplyToMessageID = msg.MessageID
+                sendConfig = msgConfig
             }
-            bot.Send(msgConfig)
+            bot.Send(sendConfig)
         } else { // non-commands, but we can generate replies to certain keywords in chat
             if msg.Text == "goat" {
                 photoConfig := telebot.NewPhotoUpload(msg.Chat.ID, "./goat.jpg")
@@ -207,14 +213,16 @@ func CaptionImage(filepath, topText, botText string) error {
 
     // top caption
     // captions should be bounded by original image's dimensions
-    topCaptionWand, err := DrawCaption(mWand.GetImageWidth(), mWand.GetImageHeight()/4, topText, true)
+    topCaptionWand, err := DrawCaption(mWand.GetImageWidth(), mWand.GetImageHeight()/3, topText, true)
+    defer topCaptionWand.Destroy()
     if err != nil {
         return errors.New("Failed to draw top caption")
     }
     mWand.CompositeImageGravity(topCaptionWand, magick.COMPOSITE_OP_OVER, magick.GRAVITY_NORTH)
 
     // bot caption
-    botCaptionWand, err := DrawCaption(mWand.GetImageWidth(), mWand.GetImageHeight()/4, botText, false)
+    botCaptionWand, err := DrawCaption(mWand.GetImageWidth(), mWand.GetImageHeight()/3, botText, false)
+    defer botCaptionWand.Destroy()
     if err != nil {
         return errors.New("Failed to draw top caption")
     }
