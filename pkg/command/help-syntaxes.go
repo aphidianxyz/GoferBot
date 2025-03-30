ee346375567c78135e5eeaeb9d15dea3ff91dc1f
aphidianxyz <aphidiangithub@proton.me>
2025-03-30 16:50:17 -0400

    add about syntax, simplify generating a helpcommand

--- pkg/command/help-syntaxes.go
+++ pkg/command/help-syntaxes.go
@@ -1,6 +1,7 @@
 package command
 
 const (
+	aboutSyntax string = "/about - describes GoferBot"
     helloSyntax string = "/hello - Gofer greets you!"
     helpSyntax string = "/help [command?] - Describes command functionality and syntax\ncommand (optional) - a specific command to describe"
     pingSyntax string = "/ping - Bot responds with 'pong'"
@@ -9,5 +10,5 @@ const (
     everyoneSyntax string = "/everyone [message?]\nmessage (optional) - a message that accompanies a ping to everyone"
 )
 
-var allHelpSyntaxes = []string{helloSyntax, helpSyntax, pingSyntax, captionSyntax, captionImgSyntax, everyoneSyntax}
+var allHelpSyntaxes = []string{aboutSyntax, helloSyntax, helpSyntax, pingSyntax, captionSyntax, captionImgSyntax, everyoneSyntax}
 
