14874d08c15af424cdffa8a0013dc496da7839d5
aphidianxyz <aphidiangithub@proton.me>
2025-04-18 20:03:57 -0400

    add error cleanup for pincommand

--- pkg/command/pin-command.go
+++ pkg/command/pin-command.go
@@ -2,6 +2,7 @@ package command
 
 import (
 	"strings"
+	"time"
 
 	telebot "github.com/OvyFlash/telegram-bot-api"
 )
@@ -10,12 +11,15 @@ type PinCommand struct {
 	api *telebot.BotAPI
 	msg telebot.Message
 	sendConfig telebot.Chattable
+	failed bool
 }
 
 func (pc *PinCommand) GenerateMessage() {
 	var replyTargetID int
+	pc.failed = false 
 	if pc.msg.ReplyToMessage == nil {
 		pc.sendConfig = telebot.NewMessage(pc.msg.Chat.ID, "Please reply to the message you want to pin, with /pin\nReplying to messages before the bot was added without visible chat history will also not work.")
+		pc.failed = true
 		return
 	}
 	replyTargetID = pc.msg.ReplyToMessage.MessageID
@@ -26,6 +30,16 @@ func (pc *PinCommand) GenerateMessage() {
 }
 
 func (pc PinCommand) SendMessage(api *telebot.BotAPI) error {
+	if pc.failed { // HACK: don't like how we're making sendmessage handle this specific case
+		var msg telebot.Message
+		var err error
+		if msg, err = api.Send(pc.sendConfig); err != nil {
+			return err
+		}
+		deleteMessage(api, 10 * time.Second, msg.Chat.ID, pc.msg.MessageID)
+		deleteMessage(api, 10 * time.Second, msg.Chat.ID, msg.MessageID)
+		return nil
+	}
     if _, err := api.Request(pc.sendConfig); err != nil {
         return err
     }
